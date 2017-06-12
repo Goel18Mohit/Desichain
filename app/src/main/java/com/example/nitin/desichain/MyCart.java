9d026658053347b5291e2a17cfe8e539139d4a89
Ashish Chudhary <ashish.choudhary2702@gmail.com>
2017-06-12 17:22:22 +0530

    Changed MyCart part 2

--- app/src/main/java/com/example/nitin/desichain/MyCart.java
+++ app/src/main/java/com/example/nitin/desichain/MyCart.java
@@ -3,28 +3,48 @@ package com.example.nitin.desichain;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MyCart extends AppCompatActivity {
+public class MyCart extends AppCompatActivity implements View.OnClickListener {
 
-    private Button mContinueBtn;
+    private TextView textCheckOut, textRupeeSymbol, textCost;
+    private ImageView imgCheckout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_cart);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+     /*   Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-/*
-        mContinueBtn = (Button) findViewById(R.id.myCartContinueBtn);
-        mContinueBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MyCart.this,BuyNow.class));
-            }
-        });
-  */  }
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+*/
+        textCheckOut = (TextView) findViewById(R.id.checkoutText);
+        textRupeeSymbol = (TextView) findViewById(R.id.rupeeSymbolCheckout);
+        textCost = (TextView) findViewById(R.id.costCheckout);
+        imgCheckout = (ImageView) findViewById(R.id.imgCheckoutForward);
 
+        textCheckOut.setOnClickListener(this);
+        textCost.setOnClickListener(this);
+        textRupeeSymbol.setOnClickListener(this);
+        imgCheckout.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.checkoutText:
+            case R.id.rupeeSymbolCheckout:
+            case R.id.costCheckout:
+            case R.id.imgCheckoutForward:
+                startActivity(new Intent(MyCart.this,BuyNow.class));
+                break;
+
+        }
+    }
 }
