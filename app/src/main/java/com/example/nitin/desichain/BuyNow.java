36946d7f7f6785849fb91bafe458d7419d523b28
nitin singhal <singhal95nitin@gmail.com>
2017-06-12 11:52:08 +0530

    my account testing

--- app/src/main/java/com/example/nitin/desichain/BuyNow.java
+++ app/src/main/java/com/example/nitin/desichain/BuyNow.java
@@ -1,11 +1,23 @@
 package com.example.nitin.desichain;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class BuyNow extends AppCompatActivity {
+
+    Button b1;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,18 +27,39 @@ public class BuyNow extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Delivery");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        b1 = (Button) findViewById(R.id.myCartContinueBtn);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(getApplicationContext(),Payment.class);
+                startActivity(i);
+            }
+        });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
 
-            case android.R.id.home: finish();
+            case android.R.id.home:
+                finish();
                 break;
 
         }
         return super.onOptionsItemSelected(item);
 
     }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+
+
+
+
 }
