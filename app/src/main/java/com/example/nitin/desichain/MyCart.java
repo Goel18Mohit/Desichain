3a0cb37a1875125775e019f24e6aa84cdb0c3b49
Ashish Chudhary <ashish.choudhary2702@gmail.com>
2017-06-12 17:36:31 +0530

    Changed MyCart part 3

--- app/src/main/java/com/example/nitin/desichain/MyCart.java
+++ app/src/main/java/com/example/nitin/desichain/MyCart.java
@@ -5,12 +5,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MyCart extends AppCompatActivity implements View.OnClickListener {
+public class MyCart extends AppCompatActivity {
 
-    private TextView textCheckOut, textRupeeSymbol, textCost;
-    private ImageView imgCheckout;
+    private LinearLayout mCheckoutLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,30 +21,14 @@ public class MyCart extends AppCompatActivity implements View.OnClickListener {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 */
-        textCheckOut = (TextView) findViewById(R.id.checkoutText);
-        textRupeeSymbol = (TextView) findViewById(R.id.rupeeSymbolCheckout);
-        textCost = (TextView) findViewById(R.id.costCheckout);
-        imgCheckout = (ImageView) findViewById(R.id.imgCheckoutForward);
-
-        textCheckOut.setOnClickListener(this);
-        textCost.setOnClickListener(this);
-        textRupeeSymbol.setOnClickListener(this);
-        imgCheckout.setOnClickListener(this);
-
-    }
-
-    @Override
-    public void onClick(View v) {
-
-        switch (v.getId()){
-
-            case R.id.checkoutText:
-            case R.id.rupeeSymbolCheckout:
-            case R.id.costCheckout:
-            case R.id.imgCheckoutForward:
+        mCheckoutLayout=(LinearLayout)findViewById(R.id.checkoutLayout);
+        mCheckoutLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 startActivity(new Intent(MyCart.this,BuyNow.class));
-                break;
+            }
+        });
 
-        }
     }
+
 }
